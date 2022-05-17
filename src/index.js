6275564656292e2bfad9a193173c9ead9bd37c5e
Kingkoara <93708295+Kingkouara@users.noreply.github.com>
2022-05-17 13:30:40 +0900

    add-list

--- src/index.js
+++ src/index.js
@@ -15,8 +15,22 @@ const onClickAdd = () => {
   //li.innerTextでli入力した値を格納する
   li.innerText = inputText;
 
+  //button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {});
+
+  //button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert();
+  });
+
   //divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了のリストに追加
   document.getElementById("incomplete-list").appendChild(div);
